e87536f0dcf23d0b304b917180bf1c08e3812442
Karen Murillo <murillok167@gmail.com>
2023-10-03 20:32:07 -0600

    variable.py

---
+++ variable.py
@@ -0,0 +1,95 @@
+#Variable de Tipo Entero
+in_num_entero=50
+print(in_num_entero)
+in_num_entero=75
+print(in_num_entero)
+in_num_entero=100
+print(in_num_entero)
+
+#Variable de Tipo Real
+num_real=3.14159
+print(num_real)
+num_real=9.16167
+print(num_real)
+num_real=18.25267
+print(num_real)
+
+#Variable de Tipo String
+tipo_cadena="Hola soy una Cadena"
+print(tipo_cadena)
+tipo_cadena="Hola Carlos"
+print(tipo_cadena)
+tipo_cadena="Hola como estas"
+print(tipo_cadena)
+
+#Variable de Tipo Bool
+tipo_bool=True
+print(tipo_bool)
+tipo_bool=False
+print(tipo_bool)
+
+#Concatenar una cadena y un entero
+numero=25
+cadena= "25" + str(30)
+print(cadena)
+
+numero=10
+cadena="5" + str(2)
+print(cadena)
+
+numero=15
+cadena="3" + str(1)
+print(cadena)
+
+#Concatenar una cadena y un flotante
+pi=8.1516
+cadena="8.1516" + str(8.1516)
+print(cadena)
+
+pi=25.2525
+cadena="15.2525" + str(10.2525)
+print(cadena)
+
+#Concatenar varias variables en una cadena
+nombre = "Ana"
+Edad= 25
+Salario = 25000
+cadena= f"nombre: {nombre}, edad: {Edad}, Salario: {Salario}"
+print(cadena)
+
+nombre = "Marcela"
+Edad= 30
+Salario = 45000
+cadena= f"nombre: {nombre}, edad: {Edad}, Salario: {Salario}"
+print(cadena)
+
+# Python no tiene límite definido para los enteros, pero está limitado por la memoria de la máquina.
+print("Límite de los enteros en Python: No hay límite definido, pero depende de la memoria.")
+
+# Los flotantes tienen límites definidos, pueden representar números muy grandes o muy pequeños, pero tienen precisión limitada.
+print("Los flotantes tienen límites definidos, pueden representar números muy grandes o muy pequeños, pero tienen precisión limitada.")
+
+# Calcular la suma de los primeros n números pares
+n = 10  # Puedes cambiar este valor a cualquier número entero
+suma_pares= 20 * (10+1)
+print(suma_pares)
+print(f"La suma de los primeros {n} números pares es: {suma_pares}")
+
+suma_pares= 12 * (12+1)
+print(suma_pares)
+print(f"La suma de los primeros {n} números pares es: {suma_pares}")
+
+suma_pares= 6 * (18+1)
+print(suma_pares)
+print(f"La suma de los primeros {n} números pares es: {suma_pares}")
+
+
+
+
+
+
+
+
+
+
+
